b56b50a61bd60c8ebc473504a97cde4b0460d6a1
willie-client <2421826956@qq.com>
2022-09-16 17:13:20 +0800

    build: main project

--- WebCourse/src/WEB_20220910/loginServlet.java
+++ WebCourse/src/WEB_20220910/loginServlet.java
@@ -1,8 +1,10 @@
+package WEB_20220910;
 
 import javax.servlet.*;
 import javax.servlet.http.*;
 import javax.servlet.annotation.*;
 import java.io.IOException;
+import java.io.PrintWriter;
 
 @WebServlet(name = "loginServlet", value = "/loginServlet")
 public class loginServlet extends HttpServlet {
@@ -13,6 +15,17 @@ public class loginServlet extends HttpServlet {
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
+        PrintWriter out = response.getWriter();
+        out.println("<html>");
+        out.println("<body>");
+        out.println("<h1>The First Servlet Project</h1>");
+        out.println("<h3>By Willie So on Sep. 09, 2022</h3>");
+        String username = request.getParameter("username");
+        String password = request.getParameter("password");
+        out.println("Username: " + username + "</br>");
+        out.println("Password: " + password + "</br>");
+        /**out.println("<a href=\"http://106.55.224.60:8080/\">back</a>");**/
+        out.println("</body>");
+        out.println("</html>");
     }
 }
---
+++ WebCourse/web/index.jsp
@@ -0,0 +1,34 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: Willie
+  Date: 2022/9/16
+  Time: 16:43
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Web编程技术实验</title>
+</head>
+<body>
+<h1>Welcome!</h1>
+<h3>Web编程技术实验线上测试环境<br>By Willie So</h3>
+<table border="1">
+    <tr>
+        <th>实验名称</th>
+        <th>时间</th>
+        <th>状态</th>
+    </tr>
+    <tr>
+        <td><a href="WEB_20220910/login.jsp" target="_blank">实验一：简单的Servlet与JSP</a></td>
+        <td>Sep. 09, 2022</td>
+        <td>已完成</td>
+    </tr>
+    <tr>
+        <td>实验二：HTTP请求、响应对象</td>
+        <td>Sep. 16, 2022</td>
+        <td>进行中</td>
+    </tr>
+</table>
+</body>
+</html>
