46cd04ef8b7b82019f50c6103abfd3c1c30eae99
willie-client <2421826956@qq.com>
2022-09-24 17:03:17 +0800

    add: HobbyServlet

---
+++ WebCourse/src/WEB_20220916/HobbyServlet.java
@@ -0,0 +1,31 @@
+package WEB_20220916;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+import java.io.IOException;
+
+@WebServlet(name = "HobbyServlet", value = "/HobbyServlet")
+public class HobbyServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String hobby = request.getParameter("hobby");
+
+        //根据兴趣决定响应重定向的网站
+        if (hobby != null && hobby.equals("1")) {
+            response.sendRedirect("http://www.csdn.net/?ref=toolbar");
+        } else if (hobby != null && hobby.equals("2")) {
+            response.sendRedirect("http://news.163.com/");
+        } else if (hobby != null && hobby.equals("3")) {
+            response.sendRedirect("http://www.4399.com/");
+        } else {//如果没有填写兴趣选项，则向客户端发送状态码和错误信息
+            response.sendError(404, "没有填写兴趣选项");
+        }
+    }
+
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+}
