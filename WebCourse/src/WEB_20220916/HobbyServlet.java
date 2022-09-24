ccadee1ca272a1ec90bea76908850c924129ebc3
willie-client <2421826956@qq.com>
2022-09-24 17:07:55 +0800

    add: HobbyServlet

--- WebCourse/src/WEB_20220916/HobbyServlet.java
+++ WebCourse/src/WEB_20220916/HobbyServlet.java
@@ -17,7 +17,7 @@ public class HobbyServlet extends HttpServlet {
         } else if (hobby != null && hobby.equals("2")) {
             response.sendRedirect("http://news.163.com/");
         } else if (hobby != null && hobby.equals("3")) {
-            response.sendRedirect("http://www.baidu.com/");
+            response.sendRedirect("http://www.4399.com/");
         } else {//如果没有填写兴趣选项，则向客户端发送状态码和错误信息
             response.sendError(404, "没有填写兴趣选项");
         }
