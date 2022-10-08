c63b2a7557c9c79daf75b96bbd40fcfcd00344a4
willie-client <2421826956@qq.com>
2022-10-08 21:22:55 +0800

    modify:format

--- WebCourse/src/WEB_20220930/FirstServlet.java
+++ WebCourse/src/WEB_20220930/FirstServlet.java
@@ -9,18 +9,10 @@ import java.io.IOException;
 public class FirstServlet extends HttpServlet {
     private static final long serialVersionUID = 1L;
 
-    /**
-     * Default constructor.
-     */
     public FirstServlet() {
-        // TODO Auto-generated constructor stub
     }
 
-    /**
-     * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-     */
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        // TODO Auto-generated method stub
         String sno = request.getParameter("stuId");
         String name = request.getParameter("stuName");
         Student_0930 student = new Student_0930(sno, name);
@@ -29,11 +21,7 @@ public class FirstServlet extends HttpServlet {
         rd.forward(request, response);
     }
 
-    /**
-     * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-     */
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        // TODO Auto-generated method stub
         doGet(request, response);
     }
 }
--- WebCourse/src/WEB_20220930/SecondServlet.java
+++ WebCourse/src/WEB_20220930/SecondServlet.java
@@ -10,19 +10,11 @@ import java.io.PrintWriter;
 public class SecondServlet extends HttpServlet {
     private static final long serialVersionUID = 1L;
 
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
     public SecondServlet() {
         super();
-        // TODO Auto-generated constructor stub
     }
 
-    /**
-     * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-     */
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        // TODO Auto-generated method stub
         Student_0930 student = (Student_0930) request.getAttribute("student");
         response.setContentType("text/html;charset=UTF-8");
         PrintWriter out = response.getWriter();
@@ -32,7 +24,6 @@ public class SecondServlet extends HttpServlet {
     }
 
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        // TODO Auto-generated method stub
         doGet(request, response);
     }
 }
